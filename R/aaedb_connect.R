94ac9c17e83a909f71d54776b460a38e17c3aada
Jian Yen <jdl.yen@gmail.com>
2023-01-19 10:00:07 +1100

    add option to use keyring to store credentials

--- R/aaedb_connect.R
+++ R/aaedb_connect.R
@@ -4,7 +4,6 @@
 #'
 #' @export
 #'
-#' @importFrom rstudioapi askForPassword
 #' @importFrom DBI dbConnect dbDisconnect dbIsValid
 #' @importFrom RPostgres Postgres
 #'
@@ -14,6 +13,15 @@
 #'   which can be useful when making repeated queries to the AAEDB as
 #'   it avoids repeatedly entering user credentials. Connections to the
 #'   AAEDB require an appropriate VPN connection.
+#'
+#'   \code{aaedb_connect} will ask for a username and password each time
+#'   it is called. To save this information locally, the
+#'   \code{aaedb_key_set} function allows a user to store their details
+#'   in the system credential store (e.g. keychain). On first call,
+#'   \code{aaedb_key_set} will ask for a username and password, after
+#'   which this information will be stored and will persist once the
+#'   R session is closed. The \code{aaedb_key_delete} function can be used
+#'   to remove this saved key.
 #'
 #'   An open connection will be automatically detected by the
 #'   \code{fetch_table} and \code{fetch_query} functions.
@@ -38,7 +46,13 @@
 #' aaedb_disconnect()
 aaedb_connect <- function(...) {
 
-  # taken from: https://www.r-bloggers.com/2022/03/closing-database-connections-in-r-packages/
+  # adapted from: https://www.r-bloggers.com/2022/03/closing-database-connections-in-r-packages/
+
+  # check whether credentials are stored locally,
+  #   if not, use rstudioapi to get creds
+  pwd_method <- "keyring"
+  if (nrow(key_list("aae_db")) == 0)
+    pwd_method <- "rstudioapi"
 
   # open connection if none exists already
   if (is.null(DB_ENV$conn)) {
@@ -48,8 +62,8 @@ aaedb_connect <- function(...) {
       dbname = "aae_db",
       host = "10.110.7.134",
       port = "5432",
-      user = rstudioapi::askForPassword("Database username"),
-      password = rstudioapi::askForPassword("Database password")
+      user = get_credentials(pwd_method, type = "username"),
+      password = get_credentials(pwd_method, type = "password")
     )
 
   } else {
@@ -65,8 +79,8 @@ aaedb_connect <- function(...) {
       dbname = "aae_db",
       host = "10.110.7.134",
       port = "5432",
-      user = rstudioapi::askForPassword("Database username"),
-      password = rstudioapi::askForPassword("Database password")
+      user = get_credentials(pwd_method, type = "username"),
+      password = get_credentials(pwd_method, type = "password")
     )
 
   }
@@ -76,6 +90,30 @@ aaedb_connect <- function(...) {
 
 }
 
+#' @title Store database credentials locally
+#'
+#' @export
+#'
+#' @importFrom keyring key_set
+#'
+#' @rdname aaedb_connect
+aaedb_key_set <- function() {
+  uid <- rstudioapi::askForPassword("Database username")
+  keyring::key_set(service = "aae_db", username = uid)
+}
+
+#' @title Store database credentials locally
+#'
+#' @export
+#'
+#' @importFrom keyring key_delete
+#'
+#' @rdname aaedb_connect
+aaedb_key_delete <- function() {
+  uid <- rstudioapi::askForPassword("Database username")
+  keyring::key_delete(service = "aae_db", username = uid)
+}
+
 #' @title Close a connection to the AAEDB
 #'
 #' @export
@@ -83,6 +121,40 @@ aaedb_connect <- function(...) {
 #' @rdname aaedb_connect
 aaedb_disconnect <- function(...) {
   closeConnection(DB_ENV)
+}
+
+# internal function to ask for a password or get stored
+#   credentials
+#' @importFrom rstudioapi askForPassword
+#' @importFrom keyring key_list key_get
+get_credentials <- function(method, type) {
+
+  # check method is supported
+  if (!method %in% c("keyring", "rstudioapi"))
+    stop("method must be one of `keyring` or `rstudioapi`", call. = FALSE)
+
+  # check that type is supported
+  if (!type %in% c("username", "password"))
+    stop("type must be one of `username` or `password`", call. = FALSE)
+
+  # use rstudioapi if creds aren't stored locally
+  if (method == "rstudioapi") {
+
+    # ask for info with appropriate phrase based on type
+    id <- rstudioapi::askForPassword(paste0("Database ", type))
+
+  } else {
+
+    # otherwise can use stored credentials for user or pwd
+    id <- key_list("aae_db")[1, 2]
+    if (type == "password")
+      id <- key_get("aae_db", id)
+
+  }
+
+  # return
+  id
+
 }
 
 # internal function to check if a connection exists
